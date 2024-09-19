652e239cf753bec3a14b977854c54bf4cd6f8aad
msalgado85 <156229265+msalgado85@users.noreply.github.com>
2024-09-19 12:30:01 -0500

    Add files via upload

---
+++ Week4Challange1.py
@@ -0,0 +1,20 @@
+#################################Extracting Sub-Strings###################################
+# Extracting Sub-Strings Practice #1
+# Extract the first word of the following sentence using slicing, and display it on the screen:
+# "Controlling complexity is the essence of programming"
+# Hint: "Controlling" is 11 characters long.
+text1 = "Controlling complexity is the essence of programming"
+print(text1[0:11])
+# Extracting Sub-Strings Practice #2
+# Take every third character starting from the ninth to the end of the sentence, and print the result.
+text2 = "Never trust a computer you can't throw out a window"
+
+print(text2[9::3])
+
+# Extracting Sub-Strings Practice #3
+# Reverses the position of all the characters in the following sentence and displays the result on the screen.
+# 
+text3 = "It's great to work with computers. They don't argue, they remember everything and they don't drink your beer"
+
+print(text3[::-1])
+
